206bf51ad9474511a7b62fe8081960e7f05609b9
sunnythelucky <sunnyparkk@gmail.com>
2023-10-31 12:12:57 -0400

    update resume

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const CTA = () => {
-  return (
-    <div className="cta">
-        <a href="resume.pdf" className="btn" download='sunny_park.pdf'>
-            Resume 
-        </a>
-    </div>
-  )
-}
+	return (
+		<div className="cta">
+			<a href="resume.pdf" className="btn" download="sunny_park.pdf">
+				Resume
+			</a>
+		</div>
+	);
+};
 
-export default CTA
+export default CTA;
