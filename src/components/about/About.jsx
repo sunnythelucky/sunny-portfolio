a1b75ffe11aa7b9c5b634197580c7cf205669614
sunnythelucky <sunnyparkk@gmail.com>
2023-10-30 20:19:07 -0400

    Modify some parts of the introduction

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -9,12 +9,11 @@ const About = () => {
 	return (
 		<section id="about">
 			<h5 className="text-light">About my work in projects of</h5>
-			<h2 className="text-light">Technology, design and education</h2>
-
+			<h2 className="text-light">Game, Medical and Construction</h2>
 			<div className="container about__container">
 				<div className="about__me">
 					<div className="about__me-image">
-						<img src={Sunny} alt="André Tavares por Nathalia Millen" />
+						<img src={Sunny} alt="Sunny Park Profile" />
 					</div>
 				</div>
 				<div className="about__content">
@@ -22,24 +21,24 @@ const About = () => {
 						<article className="about__card">
 							<GiNinjaHead className="about__icon" />
 							<h5>Experience</h5>
-							<small>+4 years of experience</small>
+							<small>4+ years of experience</small>
 						</article>
 						<article className="about__card">
 							<GoOrganization className="about__icon" />
-							<h5>Clients</h5>
-							<small>Support and consultancy</small>
+							<h5>Teamwork</h5>
+							<small>Collaborative and motivate</small>
 						</article>
 						<article className="about__card">
 							<RiTeamFill className="about__icon" />
 							<h5>Projects</h5>
-							<small>Partnerships and teams</small>
+							<small>Engaged in all stages of SDLC</small>
 						</article>
 					</div>
 					<p>
-						Results-oriented software engineer with over 4 years of hands-on experience in the field, specializing in
+						Results-oriented Fullstack developer with over 4 years of hands-on experience in the field, specializing in
 						mobile responsive web development. Skilled in leading teams, planning and monitoring projects, and
 						maintaining high-quality products. Proficient in a range of technologies, including React, TypeScript,
-						JavaScript, Node.js, GraphQL, Restful API, and AWS. Demonstrated ability to successfully manage independent
+						JavaScript, Node.js, GraphQL, and RESTful APIs. Demonstrated ability to successfully manage independent
 						projects and collaborate effectively within a team setting.
 					</p>
 					<a href="#contact" className="btn btn-primary">
